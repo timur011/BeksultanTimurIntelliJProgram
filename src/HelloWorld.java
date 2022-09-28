b44b7466e46a5fd09b671c164af2a14a9a3c2324
BT_3W2_05 <timurb2@nycstudents.net>
2022-09-28 08:55:56 -0400

    added a print statement

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class HelloWorld {
+public class HelloWorld {
+    public static void main(String[] args)
+    {
+
+        System.out.println("Hello World!");
+        String firstStatement = "this is a cool statement";
+        System.out.println("This is a cool statement");
+
+    }
+
 }
